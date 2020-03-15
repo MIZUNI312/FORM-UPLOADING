8dca08c74b146e7614bb2b13eca34d3923a6d004
MIZUNI312 <60280212+MIZUNI312@users.noreply.github.com>
2020-03-16 07:27:59 +0800

    Update Uploading_Validation.php

--- Moving/Uploading_Validation.php
+++ Moving/Uploading_Validation.php
@@ -1,34 +1,44 @@
 <?php
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" ) {
-    echo "Sorry, only JPEG & PNG files are allowed.";
-    $uploadOk = 0;  
-} 
-if ($_FILES["fileToUpload"]["size"] > 1000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-        echo '<img src="uploads/'.$_FILES["fileToUpload"]["name"].'" height = 150 width = 300>';
-    } else {
-        echo "Sorry, there was an error uploading your file.";}
-    }
-?>
+    
+    if(isset($_POST['submit'])){
+ 
+        $countfiles = count($_FILES['File']['name']);
 
+        for($i=0;$i<$countfiles;$i++){
+         $name = $_FILES['File']['name'][$i];
+         $size = $_FILES['File']['size'][$i];
+         $type = $_FILES['File']['type'][$i];
+         $tmp_name = $_FILES['File']['tmp_name'][$i];
+       
+         if(checkType($type) && checkSize($size)){
+    
+        if(move_uploaded_file($_FILES['File']['tmp_name'][$i],'uploads/'.$name)){
+            echo "<img src='uploads/".$name."' width='150' height='150'>";
+
+            
+        }else{
+            echo "Failed to upload";
+        }
+    }else{
+        echo "Dili mao ang file type";
+    }
+    
+ }
+}
+
+function checkType($type = null){
+    $allowed = array('png','jpg','jpeg','gif','mp3');
+
+    if($type){
+        $strArray = explode('/', $type);
+        return in_array(end($strArray), $allowed);
+    }
+}
+
+function checkSize($size = null){
+    $max = 1000000; 
+    if($size){
+        return $size <= $max;
+    }
+}
+?>
